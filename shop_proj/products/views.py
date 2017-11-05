1962817d1209d43fe71bb6f6167b53e58128b496
Paul Cheeseman <Paul_Cheeseman@zoho.com>
2017-11-05 12:36:16 +0000

    Provisional basket listing and items removal in place

--- shop_proj/products/views.py
+++ shop_proj/products/views.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from .models import Product
+from django.db.models import Q
+
 
 # Create your views here.
 
@@ -13,37 +15,151 @@ def all_products(request):
 
 
 
+
+
+
+
 def basket_add(request):
 	product = request.POST['product']
 	amount = request.POST['amount']	
 
  	cart = request.session.get('cart', {})
- 	
- 	print amount
+ 	#print product
+ 	#print amount
 
  	if int(amount) == 0:
  		print "zero val"
-		cart.pop(product)
+ 		#remove from cart
+ 		if product in cart:
+			cart.pop(product)
+			request.session['cart'] = cart
+			#print cart 
  	else: 
  		cart[product] = amount
 		request.session['cart'] = cart
 
+	print "cart info:"
 	print request.session['cart']
-
-
-
-	for item in cart:
-		print item, cart[item]
-
-
 	products = Product.objects.all()
 	return render(request, "products/products.html", {"products": products})
 
 
+
+
+
+
+def basket_rem(request):
+	#print request.POST['product']
+	product = request.POST['product']
+	print "product to remove:"
+	print product
+	count = 0
+	basket_item = {}
+
+	#Dodgey coding
+	text_str = ""
+
+ 	basket = request.session.get('cart', {})
+ 	print "Basket:"
+	print basket
+
+
+	if product in basket:
+		basket.pop(product)
+		request.session['cart'] = basket
+		#print cart 
+
+	#is basket empty after item removed?
+	# empty dictionary returns false
+
+	if basket:
+		print "in basket"
+		for item_key in basket:
+			print "basket item"
+			print item_key
+ 			print basket[item_key]
+ 			print "-----------------"
+ 		#query_add = query_add + ""
+			basket_item[count] = Product.objects.filter(id=item_key)
+
+			if count == 0:
+ 				text_str = "products = basket_item[%d]" %(count)
+
+			else:
+				text_str = text_str + " | basket_item[%d]" %(count)
+
+	 		#print text_str
+			count += 1
+
+		#print "text_str"		
+		#print text_str
+		#Dodgey coding
+		exec text_str
+
+	 	#exec(products)
+		#print products
+		print "something in"
+		return render(request, "products/basket.html", {"products": products})
+	else:
+		print "nothing in"
+		return render(request, "products/basket.html")
+
+
+
+
+
 def basket_list(request):
 
-	products = Product.objects.all()		
-	return render(request, "products/products.html",  {"products": products})      
+	prod_item = {}
+	products = {}
+	count = 0
+	
+	#Dodgey coding?
+	text_str = ""
+
+	#print "cart"
+	#print request.session.get('cart', {})
+ 	basket = request.session.get('cart', {})
+	print basket
+
+	for item_key in basket:
+
+		#print "basket item"
+		#print item_key
+ 		#print basket[item_key]
+
+ 		#query_add = query_add + ""
+
+ 		prod_item[count] = Product.objects.filter(id=item_key)
+
+
+
+ 		if count == 0:
+ 			text_str = "products = prod_item[%d]" %(count)
+
+ 		else:
+ 			text_str = text_str + " | prod_item[%d]" %(count)
+
+ 		#print text_str
+ 		count += 1
+
+ 	#Dodgey coding
+ 	exec text_str
+
+
+	#print products
+
+
+ 	#print products
+ 	#pulling out product object where product is equal to name, adding quantity?
+
+	#for item in basket:
+		#print item, cart[item] 
+	#	print item	
+	#	print Product.objects.get(id=basket[item])
+
+	return render(request, "products/basket.html", {"products": products})
+
 
 
 
@@ -57,6 +173,7 @@ def sort_prod_alpha(request):
 	else:
 		reverse = "true"
 		sorted_products = Product.objects.order_by('name')
+
 	return render(request, "products/products.html",  {"products": sorted_products, "reverse": reverse})
 
 
--- shop_proj/shop_proj/urls.py
+++ shop_proj/shop_proj/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     url(r'^paypal-cancel', paypal_views.paypal_cancel),	
     url(r'^products/$', product_views.all_products),
     url(r'^basket_add/$', product_views.basket_add, name='basket_add'),
+    url(r'^basket_rem/$', product_views.basket_rem, name='basket_rem'),    
     url(r'^basket_list/$', product_views.basket_list, name='basket_list'),    
     url(r'^sort_prod_alpha/$', product_views.sort_prod_alpha, name='sort_prod_alpha'),
     url(r'^sort_prod_price/$', product_views.sort_prod_alpha, name='sort_prod_price'),    
