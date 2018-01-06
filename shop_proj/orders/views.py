44fde9424c013b2741fba3636489f3b5a5dc25b7
Paul Cheeseman <Paul_Cheeseman@zoho.com>
2018-01-06 18:15:20 +0000

    Orders list page issued with correct delivery charges and totals

--- shop_proj/orders/views.py
+++ shop_proj/orders/views.py
@@ -13,11 +13,8 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-print("Made it here!!!")
-
 @login_required()
 def orders_list(request):
-	print("EVEN Made it here!!!")
 
 	#identify customer
 	customer = get_object_or_404(User, username=request.user)
@@ -25,13 +22,27 @@ def orders_list(request):
 	#get all customer orders
 	order_list = Order.objects.filter(customer_id=customer)
 
+	#Switch order so most recent appears first on page
+	order_list = order_list.order_by('-id')
+
+
 	if not order_list.exists():
 		messages.info(request, "You currently have no orders to view")          
 
-
-	for order in order_list:
-		order.date = order.order_date.date()
-		order.time = order.order_date.time().strftime('%H:%M:%S')
+	else:
+		#Run through each of the customers orders and put in an individual time and date field from timedate stamp
+		for order in order_list:
+			order.date = order.order_date.date()
+			order.time = order.order_date.time().strftime('%H:%M:%S')
+		
+			#For each order, go through the associated order items and retrieve the quantities orders so can 
+			#publish the deliver amount
+			order_items = OrderItem.objects.filter(order_id=order.id)
+			item_quantity = 0
+			for item in order_items:
+				item_quantity = item_quantity + item.quantity
+			#The *2 is for the £2 delivery charge, ideally this would be a global var that would be referenced
+			order.total = order.total + (item_quantity * 2)
 
 	#Paginating output (if required)
 	page = request.GET.get('page', 1)
@@ -52,7 +63,6 @@ def orders_list(request):
 
 @login_required()
 def orders_detail(request):
-
 
 	order = 0
 	order_id = 0
