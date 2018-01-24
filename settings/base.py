72859dde208c72206be0000abe7281466f530398
Paul Cheeseman <Paul_Cheeseman@zoho.com>
2018-01-24 14:18:43 +0000

    adding whitenoise for static file serving on heroku

--- settings/base.py
+++ settings/base.py
@@ -37,6 +37,9 @@ AUTHENTICATION_BACKENDS = (
 
 
 MIDDLEWARE = [
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -96,7 +99,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # If going to add App specific img/css then use:
