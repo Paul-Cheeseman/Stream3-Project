33e04d66fa3ee7511b20b8d08ae8f14982cb2975
Paul Cheeseman <Paul_Cheeseman@zoho.com>
2018-01-24 12:47:43 +0000

    removing old files

--- shop_proj_delete/orders/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^list/$', views.orders_list, name='orders'),
-    url(r'^detail/$', views.orders_detail, name='orders_detail'),        
-]
-
-
--- shop_proj_delete/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^list/$', views.products, name='products'),
-    url(r'^detail/$', views.product_detail, name='product_detail'),    
-]
-
-
