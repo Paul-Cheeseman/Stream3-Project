62b162cac064479e9a69c9de06f2fa9531af0ffd
Paul Cheeseman <Paul_Cheeseman@zoho.com>
2018-01-25 18:59:04 +0000

    changing media root again

--- settings/staging.py
+++ settings/staging.py
@@ -47,8 +47,33 @@ LOGGING = {
     },
 }
 
+#Compressing static files
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
+AWS_STORAGE_BUCKET_NAME = 'stream3img'
+AWS_S3_REGION_NAME = 'eu-west-2'  # e.g. us-east-2
+AWS_ACCESS_KEY_ID = 'AKIAIRSALN3EJ2Y4DZLQ'
+AWS_SECRET_ACCESS_KEY = '+DVvnwnuuul+k4AdKW8dYgSjKjWaH9Tg0LcY6raI'
+
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = 's3.%s.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+#https://s3.stream3img.amazonaws.com/
+#https://eu-west-2.s3.stream3img.amazonaws.com/
+
+#https://s3.eu-west-2.amazonaws.com/stream3img/Yellow_Trousers_2.jpg
+#MEDIA_ROOT = "https://%s.%s/" % (AWS_S3_REGION_NAME, AWS_S3_CUSTOM_DOMAIN)
+THUMBNAIL_MEDIA_ROOT = 'https://s3.eu-west-2.amazonaws.com/stream3img/'
+THUMBNAILMEDIA_URL = 'media/'
+
+print("media ROOT")
+print(MEDIA_ROOT)
+
+
+#Point media files to Amazon storage files
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 
 
