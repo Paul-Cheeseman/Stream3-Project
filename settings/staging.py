c15afcd8fb09d16f77d4a3b3ca14c322b20cb130
Paul Cheeseman <Paul_Cheeseman@zoho.com>
2018-01-25 12:25:30 +0000

    AWS hosting removing static reference from settings file

--- settings/staging.py
+++ settings/staging.py
@@ -51,14 +51,14 @@ LOGGING = {
 }
 
 
-AWS_STORAGE_BUCKET_NAME = 'stream3img'
-AWS_S3_REGION_NAME = 'eu-west-2'  # e.g. us-east-2
-AWS_ACCESS_KEY_ID = 'AKIAIRSALN3EJ2Y4DZLQ'
-AWS_SECRET_ACCESS_KEY = '+DVvnwnuuul+k4AdKW8dYgSjKjWaH9Tg0LcY6raI'
+#AWS_STORAGE_BUCKET_NAME = 'stream3img'
+#AWS_S3_REGION_NAME = 'eu-west-2'  # e.g. us-east-2
+#AWS_ACCESS_KEY_ID = 'AKIAIRSALN3EJ2Y4DZLQ'
+#AWS_SECRET_ACCESS_KEY = '+DVvnwnuuul+k4AdKW8dYgSjKjWaH9Tg0LcY6raI'
 
 # Tell django-storages the domain to use to refer to static files.
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+#AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
 # you run `collectstatic`).
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
