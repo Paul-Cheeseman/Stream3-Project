5959ff06a3603b4cbe341999cd8ab3485591bb04
Paul Cheeseman <Paul_Cheeseman@zoho.com>
2018-01-26 08:23:20 +0000

    removal of media URL for staging

--- settings/staging.py
+++ settings/staging.py
@@ -66,15 +66,17 @@ AWS_S3_CUSTOM_DOMAIN = 's3.%s.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 #https://s3.eu-west-2.amazonaws.com/stream3img/Yellow_Trousers_2.jpg
 #MEDIA_ROOT = "https://%s.%s/" % (AWS_S3_REGION_NAME, AWS_S3_CUSTOM_DOMAIN)
-THUMBNAIL_MEDIA_ROOT = 'https://s3.eu-west-2.amazonaws.com/stream3img/media/'
-THUMBNAILMEDIA_URL = 'media/'
+#MEDIA_ROOT = "https://%s.%s/" % (AWS_S3_REGION_NAME, AWS_S3_CUSTOM_DOMAIN)
+MEDIA_URL = 'media/'
+#THUMBNAIL_MEDIA_ROOT = 'https://s3.eu-west-2.amazonaws.com/stream3img/media/'
+#THUMBNAILMEDIA_URL = 'media/'
 
 print("media ROOT")
 print(MEDIA_ROOT)
 
 
 #Point media files to Amazon storage files
-#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
 #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 
